cdaccb0d38d8f886bca412212f68c35bc94d9656
V-U-Simon <V-U-Simon@yandex.ru>
2023-09-23 13:48:12 +0300

    parse GET & POST

--- lesson_02/framework_wsgi/http/request.py
+++ lesson_02/framework_wsgi/http/request.py
@@ -1,4 +1,6 @@
 from framework_wsgi.middleware import middleware
+from urllib.parse import parse_qs
+import cgi
 
 
 class Request:
@@ -6,6 +8,9 @@ class Request:
         print(f"Processing request")
         self.environ = environ
         self.path = environ["PATH_INFO"]
+        self.method = environ["REQUEST_METHOD"].upper()
+        self.params = {}
+        self.data = {}
 
         for func in middleware.pre_process_funcs:
             func(self)
@@ -13,5 +18,42 @@ class Request:
 
 @middleware.pre_process
 def check_path_slash(request: Request):
+    # добавление закрывающего слеша
     if not request.path.endswith("/"):
         request.path = request.path + "/"
+
+
+@middleware.pre_process
+def parse_request_data(request: Request):
+    # Для GET-запросов
+    if request.method == "GET":
+        query_string = request.environ.get("QUERY_STRING", "")
+        request.params = parse_qs(query_string)
+
+    # Для POST-запросов
+    elif request.method == "POST":
+        content_type = request.environ.get("CONTENT_TYPE")
+        content_length = int(request.environ.get("CONTENT_LENGTH", 0))
+
+        # Если это обычная форма
+        if content_type == "application/x-www-form-urlencoded":
+            body = request.environ["wsgi.input"].read(content_length).decode("utf-8")
+            request.params = parse_qs(body)
+
+        # Если это multipart/form-data
+        elif content_type.startswith("multipart/form-data"):
+            form = cgi.FieldStorage(
+                fp=request.environ["wsgi.input"],
+                environ=request.environ,
+                keep_blank_values=True,
+            )
+            request.params = {}
+            request.data = {}
+
+            for key in form:
+                if form[key].filename:
+                    # Элемент является файлом
+                    request.data[key] = form[key]
+                else:
+                    # Элемент является обычным полем
+                    request.params[key] = form[key].value
