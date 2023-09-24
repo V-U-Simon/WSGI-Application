f213a0e8c97f3c6cb1eb67b45ede37cb963bf499
V-U-Simon <V-U-Simon@yandex.ru>
2023-09-24 16:49:26 +0300

    request: приведение к принципу single responsibility

--- lesson_04/framework_wsgi/http/request.py
+++ lesson_04/framework_wsgi/http/request.py
@@ -1,58 +1,55 @@
-from framework_wsgi.middleware import middleware
+from typing import Type
+
 from urllib.parse import parse_qs
 import cgi
 
 
 class Request:
     def __init__(self, environ: dict):
-        self.environ = environ
         self.url = self._prepare_url(environ["PATH_INFO"])
+        self.environ = environ
         self.method = environ["REQUEST_METHOD"].upper()
+        self.cookies = {}
+        self.headers = {k[5:]: v for k, v in environ.items() if k.startswith("HTTP_")}
 
-        self.params = {}
-        self.data = {}
+        self._fill_post_params(environ["wsgi.input"])
+        self._fill_get_params(environ["QUERY_STRING"])
 
-        for func in middleware.pre_process_funcs:
-            func(self)
-
+    @staticmethod
     def _prepare_url(url: str):
-        # добавление закрывающего слеша
         return url if url.endswith("/") else url + "/"
 
-
-@middleware.pre_process
-def parse_request_data(request: Request):
-    # Для GET-запросов
-    if request.method == "GET":
-        query_string = request.environ.get("QUERY_STRING", "")
-        request.GET = {
-            k: v[0] if len(v) == 1 else v for k, v in parse_qs(query_string).items()
+    def _fill_get_params(self, raw_params: str):
+        self.GET = {
+            k: v if len(v) == 1 else v[0] for k, v in parse_qs(raw_params).items()
         }
 
-    # Для POST-запросов
-    elif request.method == "POST":
-        content_type = request.environ.get("CONTENT_TYPE")
-        content_length = int(request.environ.get("CONTENT_LENGTH", 0))
+    def _fill_post_params(self, raw_params: bytes):
+        content_type = self.environ.get("CONTENT_TYPE")
+        content_length = int(
+            self.environ.get("CONTENT_LENGTH")
+            if bool(self.environ.get("CONTENT_LENGTH"))
+            else 0
+        )
 
-        # Если это обычная форма
-        if content_type == "application/x-www-form-urlencoded":
-            body = request.environ["wsgi.input"].read(content_length).decode("utf-8")
-            request.POST = {
-                k: v[0] if len(v) == 1 else v for k, v in parse_qs(body).items()
-            }
+        match content_type:
+            case "application/x-www-form-urlencoded":  # Если это обычная форма
+                body = raw_params.read(content_length).decode("utf-8")
+                self.POST = {
+                    k: v[0] if len(v) == 1 else v for k, v in parse_qs(body).items()
+                }
 
-        # Если это multipart/form-data
-        elif content_type.startswith("multipart/form-data"):
-            form = cgi.FieldStorage(
-                fp=request.environ["wsgi.input"],
-                environ=request.environ,
-                keep_blank_values=True,
-            )
+            case "multipart/form-data":
+                form = cgi.FieldStorage(
+                    fp=raw_params,
+                    environ=self.environ,
+                    keep_blank_values=True,
+                )
 
-            for key in form:
-                if form[key].filename:
-                    # Элемент является файлом
-                    request.FILES[key] = form[key]
-                else:
-                    # Элемент является обычным полем
-                    request.POST[key] = form[key].value
+                for key in form:
+                    if form[key].filename:
+                        # Элемент является файлом
+                        self.FILES[key] = form[key]
+                    else:
+                        # Элемент является обычным полем
+                        self.POST[key] = form[key].value
