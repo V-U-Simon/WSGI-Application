6e5b416b1b345b3234a10da8e641e87f19c5c5aa
V-U-Simon <V-U-Simon@yandex.ru>
2023-09-26 17:06:32 +0300

    mapper registry

---
+++ lesson_04/framework_wsgi/db/mapper_registry.py
@@ -0,0 +1,23 @@
+from typing import TypeVar
+
+from framework_wsgi.db.domain import Students, Teachers, Users
+
+T = TypeVar("T")
+
+
+class MapperRegistry:
+    registry: list[T] = []
+
+    @classmethod
+    def register(cls, entity_type):
+        """Регистрация нового маппера."""
+        cls._registry.append(entity_type)
+
+
+# Регистрация мапперов
+MapperRegistry.register(Users)
+MapperRegistry.register(Students)
+MapperRegistry.register(Teachers)
+
+if __name__ == "__main__":
+    print(MapperRegistry._registry)
